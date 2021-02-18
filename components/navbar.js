0cff342e96fe3fa5b9a13c3f1894ae043cccc6e3
Su988 <suyash988@gmail.com>
2021-02-18 11:00:19 -0500

    fixed modal n hamburger menu not closing on click

--- components/navbar.js
+++ components/navbar.js
@@ -1,61 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
-import ToggleTheme from './toggleTheme';
-import { FaGithub } from 'react-icons/fa';
-import styles from '../styles/Navbar.module.css';
 import Hamburger from 'hamburger-react';
+import NavList from './navList';
+import Modal from '../components/modal';
 
 export default function Navbar() {
-  return (
-    <div className='flex justify-between items-center dark:bg-gray-900 pl-4 pr-4 pt-4 pb-10 md:pl-10 md:pr-10'>
-      <Link href={'/'} as={'/'}>
-        <div className='dark:text-white text-3xl cursor-pointer'>Suyash S</div>
-      </Link>
+  const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
-      <ul className='md:flex dark:text-white text-xl items-center hidden'>
-        <Link href={'/post'} as={'/post'}>
-          <span>
-            <li
-              className={`mr-3 hover:opacity-20 transition-all cursor-pointer ${styles.my_li}`}
-            >
-              Blog
-            </li>
-          </span>
+  const toggleModal = () => {
+    setShowModal((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    return () => {
+      setShowModal(false);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className='flex justify-between items-center dark:bg-gray-900 pl-4 pr-4 pt-4 pb-10 md:pl-10 md:pr-10'>
+        <Link href={'/'} as={'/'}>
+          <div className='dark:text-white text-3xl cursor-pointer'>
+            Suyash S
+          </div>
         </Link>
-        <span>
-          <li
-            className={`mr-3 hover:opacity-20 transition-all cursor-pointer ${styles.my_li}`}
-          >
-            Projects
-          </li>
-        </span>
-        <span>
-          <li
-            className={`mr-3 hover:opacity-20 transition-all cursor-pointer ${styles.my_li}`}
-          >
-            <a href='mailto:suyashvks@gmail.com'>Contact</a>
-          </li>
-        </span>
-        <span>
-          <li
-            className={`mr-3 hover:opacity-20 transition-all cursor-pointer ${styles.my_li}`}
-          >
-            About me
-          </li>
-        </span>
-        <li className={`mr-3`}>|</li>
-        <li className='mr-4 hover:opacity-20 transition-all cursor-pointer'>
-          <a href={'https://github.com/su988'} target='_blank'>
-            <FaGithub />
-          </a>
-        </li>
-        <li className='cursor-pointer'>
-          <ToggleTheme />
-        </li>
-      </ul>
-      <div className='md:hidden dark:text-white'>
-        <Hamburger easing='ease-in' />
+
+        <ul className='md:flex dark:text-white text-xl items-center hidden'>
+          <NavList />
+        </ul>
+        <div className='md:hidden dark:text-white z-50'>
+          <Hamburger
+            toggled={isOpen}
+            toggle={setIsOpen}
+            onToggle={(toggled) => {
+              if (toggled) {
+                setShowModal(true);
+              } else {
+                setShowModal(false);
+              }
+            }}
+          />
+        </div>
       </div>
-    </div>
+      {showModal && (
+        <Modal>
+          <ul className=''>
+            <NavList
+              loc={'mobile'}
+              toggleModal={toggleModal}
+              hamburgerOpen={setIsOpen}
+            />
+          </ul>
+        </Modal>
+      )}
+    </>
   );
 }
