41327df2ffff5c67ed9aa45c04355cebf78a3393
Su988 <suyash988@gmail.com>
2021-02-23 10:41:35 -0500

    fadeInSection component added

--- components/skills.js
+++ components/skills.js
@@ -8,59 +8,61 @@ import { FaNode } from 'react-icons/fa';
 import { SiPostgresql } from 'react-icons/si';
 import { SiFirebase } from 'react-icons/si';
 import { SiRedux } from 'react-icons/si';
-import styles from '../styles/Landing.module.css';
+import FadeInSection from '../components/fadeInSection';
 
 export default function Skills() {
   return (
-    <div className={`mt-24 mx-5 md:mx-20 lg:mx-32 pb-20 ${styles.skills}`}>
+    <div className={`mt-24 mx-5 md:mx-20 lg:mx-32 pb-20`}>
       <h4 className={`text-5xl lg:text-8xl font-black mb-10`}>Skills</h4>
-      <div className='flex flex-wrap justify-center p-4 lg:mt-20'>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
-        >
-          <FaHtml5 />
+      <FadeInSection>
+        <div className='flex flex-wrap justify-center p-4 lg:mt-20'>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
+          >
+            <FaHtml5 />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400 `}
+          >
+            <FaCss3 />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
+          >
+            <SiTailwindcss />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-3xl text-gray-400`}
+          >
+            <SiJavascript />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
+          >
+            <FaReact />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-2xl md:text-5xl text-gray-400`}
+          >
+            <SiRedux />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-2xl md:text-5xl text-gray-400`}
+          >
+            <SiFirebase />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
+          >
+            <FaNode />
+          </div>
+          <div
+            className={`flex justify-center min-w-1/3 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
+          >
+            <SiPostgresql />
+          </div>
         </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400 `}
-        >
-          <FaCss3 />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
-        >
-          <SiTailwindcss />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-3xl text-gray-400`}
-        >
-          <SiJavascript />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
-        >
-          <FaReact />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-2xl md:text-5xl text-gray-400`}
-        >
-          <SiRedux />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-2xl md:text-5xl text-gray-400`}
-        >
-          <SiFirebase />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 mb-10 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
-        >
-          <FaNode />
-        </div>
-        <div
-          className={`flex justify-center min-w-1/3 lg:mb-20 text-3xl md:text-5xl text-gray-400`}
-        >
-          <SiPostgresql />
-        </div>
-      </div>
+      </FadeInSection>
     </div>
   );
 }
