ec6ccfa0d4e81c618c3be1dcd553cbd47f85fd1a
Su988 <suyash988@gmail.com>
2021-02-24 11:22:24 -0500

    restyled project mobile version overlay

--- components/project.js
+++ components/project.js
@@ -18,16 +18,9 @@ export default function Project() {
           <div
             className={`h-60 mb-4 lg:mb-0 lg:border-r-2 lg:min-w-1/2 lg:h-96 bg-project-chat bg-cover relative ${styles.container}`}
           >
-            <a
-              href='https://github.com/su988/react_firebase_chat_app'
-              target='blank'
-              className={`absolute bottom-5 right-5 text-3xl md:hidden text-gray-100`}
-            >
-              <FaGithub />
-            </a>
             <div className={`${styles.overlay} bg-gray-900 dark:bg-gray-800`}>
               <p
-                className={`text-gray-100 tracking-widest leading-normal lg:leading-loose m-2 lg:m-6`}
+                className={`text-base text-gray-100 tracking-widest leading-normal lg:leading-loose m-2 lg:m-6`}
               >
                 Real time chat app where users can either create their own
                 channel or join an existing one. Built with ReactJS for frontend
@@ -46,16 +39,9 @@ export default function Project() {
           <div
             className={`h-60 mb-4 lg:mb-0 lg:border-l-2 lg:min-w-1/2 lg:h-96 bg-gray-800 bg-project-github bg-cover relative ${styles.container}`}
           >
-            <a
-              href='https://github.com/su988/github_jobs'
-              target='blank'
-              className={`absolute bottom-5 right-5 text-3xl md:hidden`}
-            >
-              <FaGithub />
-            </a>
             <div className={`${styles.overlay} bg-gray-900 dark:bg-gray-800`}>
               <p
-                className={`text-gray-100 tracking-widest leading-normal lg:leading-loose m-6`}
+                className={`text-base text-gray-100 tracking-widest leading-normal lg:leading-loose m-6`}
               >
                 Job search website which pulls data from Github Jobs api. Built
                 with React using Hooks and context for state management.
@@ -73,16 +59,9 @@ export default function Project() {
           <div
             className={`h-60 mb-4 lg:mb-0 lg:border-t-2 lg:border-r-2 lg:min-w-1/2 lg:h-96 bg-gray-800 bg-gray-800 bg-project-fpl bg-contain md:bg-cover lg:bg-cover relative ${styles.container}`}
           >
-            <a
-              href='https://github.com/su988/fpli_api'
-              target='blank'
-              className={`absolute bottom-5 right-5 text-3xl md:hidden text-white`}
-            >
-              <FaGithub />
-            </a>
             <div className={`${styles.overlay} bg-gray-900 dark:bg-gray-800`}>
               <p
-                className={`text-gray-100 tracking-widest leading-normal lg:leading-loose m-2 lg:m-6`}
+                className={`text-base text-gray-100 tracking-widest leading-normal lg:leading-loose m-2 lg:m-6`}
               >
                 Dashboard tracking Fantasy Premiere League data. Python request
                 to get data from FPL API. Pandas library to clean data and
@@ -99,7 +78,7 @@ export default function Project() {
               <a
                 href='https://fpl-dash.herokuapp.com/'
                 target='blank'
-                className={`absolute bottom-4 right-24 sm:text-xl lg:text-3xl text-gray-100`}
+                className={`absolute right-20 bottom-5 md:bottom-4 md:right-24 sm:text-xl lg:text-3xl text-gray-100`}
               >
                 <MdLaptopMac />
               </a>
@@ -109,16 +88,9 @@ export default function Project() {
           <div
             className={`h-60 lg:mb-0 lg:border-t-2 lg:border-l-2 lg:min-w-1/2 lg:h-96 bg-gray-800 bg-project-address bg-contain md:bg-cover lg:bg-contain relative ${styles.container}`}
           >
-            <a
-              href='https://github.com/su988/address_book_js'
-              target='blank'
-              className={`absolute bottom-5 right-5 text-3xl md:hidden text-gray-100`}
-            >
-              <FaGithub />
-            </a>
             <div className={`${styles.overlay} bg-gray-900 dark:bg-gray-800`}>
               <p
-                className={`text-gray-100 tracking-widest leading-normal lg:leading-loose m-6`}
+                className={`text-base text-gray-100 tracking-widest leading-normal lg:leading-loose m-6`}
               >
                 A minimalist address book built with vanilla Javascript.
                 Persists data into local storage.
@@ -133,7 +105,7 @@ export default function Project() {
               <a
                 href='https://modest-curran-ab2d49.netlify.app/'
                 target='blank'
-                className={`absolute bottom-4 right-24 sm:text-xl lg:text-3xl text-gray-100`}
+                className={`absolute right-20 bottom-5 md:bottom-4 md:right-24 sm:text-xl lg:text-3xl text-gray-100`}
               >
                 <MdLaptopMac />
               </a>
