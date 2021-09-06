282fa1b8da698cb6c8267c73e475eebc6176f618
Suyash S <suyash988@gmail.com>
2021-09-06 18:10:00 -0400

    changed font

--- pages/_app.js
+++ pages/_app.js
@@ -22,7 +22,7 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700;900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap"
           rel="stylesheet"
         />
       </Head>
